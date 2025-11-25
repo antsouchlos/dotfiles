7be47bb060b61184aaca94fa6b5375b4a33a3d38
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2025-11-25 18:39:38 +0100

    nvim: Formatting; Update plugins

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,26 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        branxh = "main",
-        lazy = false, -- This plugin doesn't support lazy loading
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "cpp",
-                    "c",
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "query"
-                },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = {
-                        "latex"
-                    }
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    branxh = "main",
+    lazy = false,     -- This plugin doesn't support lazy loading
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "cpp",
+                "c",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query"
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = {
+                    "latex"
                 }
-            })
-        end
-    }
+            }
+        })
+    end
 }
