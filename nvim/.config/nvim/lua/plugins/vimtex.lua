2ff2a7c282a5f6aef53ec83f650634a1ed301024
Andreas Tsouchlos <andreas.tsouchlos@student.kit.edu>
2025-12-01 20:14:08 +0100

    Enable concealment only for latex files

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -23,26 +23,31 @@ return {
 
         -- Concealment
 
-        vim.o.conceallevel = 1
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = 'latex',
+            callback = function()
+                vim.o.conceallevel = 1
 
-        vim.g.vimtex_syntax_conceal = {
-            accents         = 1,
-            ligatures       = 1,
-            cites           = 1,
-            fancy           = 1,
-            spacing         = 0,
-            greek           = 1,
-            math_bounds     = 1,
-            math_delimiters = 1,
-            math_fracs      = 1,
-            math_super_sub  = 0,
-            math_symbols    = 1,
-            sections        = 0,
-            styles          = 0,
-        }
+                vim.g.vimtex_syntax_conceal = {
+                    accents         = 1,
+                    ligatures       = 1,
+                    cites           = 1,
+                    fancy           = 1,
+                    spacing         = 0,
+                    greek           = 1,
+                    math_bounds     = 1,
+                    math_delimiters = 1,
+                    math_fracs      = 1,
+                    math_super_sub  = 0,
+                    math_symbols    = 1,
+                    sections        = 0,
+                    styles          = 0,
+                }
 
-        vim.g.vimtex_syntax_custom_cmds = {
-            { name = 'bm', mathmode = 1, argstyle = 'bold', conceal = 1 },
-        }
+                vim.g.vimtex_syntax_custom_cmds = {
+                    { name = 'bm', mathmode = 1, argstyle = 'bold', conceal = 1 },
+                }
+            end
+        })
     end
 }
