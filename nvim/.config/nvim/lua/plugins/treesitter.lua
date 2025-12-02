15e9eaba8f2b1c631436e8e8a7d4db53cf4b91fa
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2025-12-02 01:43:04 +0100

    Add custom textobjects and keymaps for treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,55 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    branxh = "main",
-    lazy = false,     -- This plugin doesn't support lazy loading
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "cpp",
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "query"
-            },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = {
-                    "latex"
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+        build = ":TSUpdate",
+        branch = "main",
+        lazy = false, -- This plugin doesn't support lazy loading
+        config = function()
+            require("nvim-treesitter").setup()
+
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    move = {
+                        enable = true,
+                        set_jumps = false,
+                        goto_next_start = {
+                            ["]c"] = { query = "@code_cell.inner", desc = "next code block" },
+                            ["]j"] = { quer = "@cellseparator", desc = "next cell" },
+                        },
+                        goto_previous_start = {
+                            ["[c"] = { query = "@code_cell.inner", desc = "previous code block" },
+                            ["[j"] = { query = "@cellseparator", desc = "next code block" },
+                        },
+                    },
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["ic"] = { query = "@code_cell.inner", desc = "in code block" },
+                            ["ac"] = { query = "@code_cell.outer", desc = "around code block" },
+                            ["aj"] = { query = "@cell", desc = "cell" },
+                            ["ij"] = { query = "@cellcontent", desc = "cell content" },
+                        },
+                    },
+                },
+                ensure_installed = {
+                    "cpp",
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query"
+                },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = {
+                        "latex"
+                    }
                 }
-            }
-        })
-    end
+            })
+        end
+    }
 }
